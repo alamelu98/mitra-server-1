fcbcf50c863890f61ec44ef87a1c1d9d3479eba1
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2023-03-23 20:33:35 +0530

    Update UserLogin.js

--- models/UserLogin.js
+++ models/UserLogin.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const mongoose=require("mongoose")
 const jwt=require("jsonwebtoken")
 const bcrypt=require("bcryptjs")
-const userLogin=mongoose.Schema({
+const UserLogin=mongoose.Schema({
     name:{
         type:String,
         required:false,
@@ -33,23 +33,23 @@ const userLogin=mongoose.Schema({
         required:[true,"Date added"]
     }
 })
-userLogin.pre("save",async function(next)
+UserLogin.pre("save",async function(next)
 {
     const salt=await bcrypt.genSalt(10)
     this.password=await bcrypt.hash(this.password,salt)
     next()
 })
-userLogin.methods.compareUserPassword=async function(userPassword)
+UserLogin.methods.compareUserPassword=async function(userPassword)
 {
     const isSame= await bcrypt.compare(userPassword,this.password)
     return isSame
 }
 
-userLogin.methods.getToken=function()
+UserLogin.methods.getToken=function()
 {
     const token=jwt.sign({userid:this._id,userEmail:this.email,Username:this.name},process.env.JWT_SECRET_USER,{expiresIn:process.env.JWT_EXPIRY})
     return token
 }
 
 
-module.exports=mongoose.model("userLogin",userLogin)
+module.exports=mongoose.model("userLogin",UserLogin)
