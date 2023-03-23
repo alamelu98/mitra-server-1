2c7ac22243d973bc20970628cfa5a5ded5c3dd0b
alamelu98 <63118650+alamelu98@users.noreply.github.com>
2023-03-23 20:28:18 +0530

    Update usersignup.js

--- controller/usersignup.js
+++ controller/usersignup.js
@@ -31,7 +31,7 @@ const userLoginin=asyncWrapper(async(req,res)=>
     }
     const token=user.getToken()
    
-    res.status(200).json({message:"Successfully loggedn in",userToken:token,userdetails_user:user,isSame:isSame})
+    res.status(200).json({message:"Successfully loggedn in",userToken:token,userdetails_user:user,isSameuser:isSameuser})
 
 })
 const newUserRegister=asyncWrapper(async(req,res)=>
